017f4edd9876218a9cda6d7226191a30010e41eb
nflores12 <66344991+nflores12@users.noreply.github.com>
2020-09-29 15:38:46 -0300

    Se agregan las pantallas con las preguntas del test
    Se agregan las pantallas con las preguntas del test

--- Femina/app/src/main/java/com/alejandro/android/femina/MainActivity.java
+++ Femina/app/src/main/java/com/alejandro/android/femina/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setItemIconTintList(null);//permite visualizar bien los iconos del menu
     }
 
     @Override
