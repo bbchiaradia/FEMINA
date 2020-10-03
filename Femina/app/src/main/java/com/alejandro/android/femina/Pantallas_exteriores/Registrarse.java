59d1655567a7bbc6396b03bffa81bfab1c194ca5
Alejandro081998 <ale.salgado08@hotmail.com>
2020-10-03 03:33:04 -0300

    Boton Ingresar y Registrarse derivan a inicio
    Boton Ingresar y Registrarse derivan a inicio

--- Femina/app/src/main/java/com/alejandro/android/femina/Pantallas_exteriores/Registrarse.java
+++ Femina/app/src/main/java/com/alejandro/android/femina/Pantallas_exteriores/Registrarse.java
@@ -2,15 +2,20 @@ package com.alejandro.android.femina.Pantallas_exteriores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
+import com.alejandro.android.femina.Main.MainActivity;
 import com.alejandro.android.femina.R;
 
 public class Registrarse extends AppCompatActivity {
 
     private Spinner sexo;
+    private Button btn_registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +23,20 @@ public class Registrarse extends AppCompatActivity {
         setContentView(R.layout.pantalla_registrarse);
 
         sexo = (Spinner) findViewById(R.id.spn_sexo_registrarse);
+        btn_registro = (Button) findViewById(R.id.btn_confirmar_registro);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sexo_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         sexo.setAdapter(adapter);
+
+        btn_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
