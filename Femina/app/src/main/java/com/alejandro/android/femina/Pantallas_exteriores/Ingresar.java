839157e93d9f57f92031eee2171657899103fe5e
Alejandro081998 <ale.salgado08@hotmail.com>
2020-10-03 03:19:19 -0300

    Pantallas ingresar y registrarse
    Pantallas ingresar y registrarse diseño y navegacion

--- Femina/app/src/main/java/com/alejandro/android/femina/Pantallas_exteriores/Ingresar.java
+++ Femina/app/src/main/java/com/alejandro/android/femina/Pantallas_exteriores/Ingresar.java
@@ -1,4 +1,44 @@
 package com.alejandro.android.femina.Pantallas_exteriores;
 
-public class Ingresar {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.alejandro.android.femina.Main.MainActivity;
+import com.alejandro.android.femina.R;
+
+public class Ingresar extends AppCompatActivity {
+
+    private Button btn_registrarse,btn_ingresar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_ingresar);
+
+       btn_ingresar = (Button) findViewById(R.id.btn_ingresar);
+       btn_registrarse = (Button) findViewById(R.id.btn_registrarse);
+
+       btn_ingresar.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+               startActivity(intent);
+           }
+       });
+
+        btn_registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Registrarse.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
 }
--- Femina/app/src/main/java/com/alejandro/android/femina/Pantallas_exteriores/Registrarse.java
+++ Femina/app/src/main/java/com/alejandro/android/femina/Pantallas_exteriores/Registrarse.java
@@ -1,4 +1,28 @@
 package com.alejandro.android.femina.Pantallas_exteriores;
 
-public class Registrarse {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.alejandro.android.femina.R;
+
+public class Registrarse extends AppCompatActivity {
+
+    private Spinner sexo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_registrarse);
+
+        sexo = (Spinner) findViewById(R.id.spn_sexo_registrarse);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sexo_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        sexo.setAdapter(adapter);
+
+    }
 }
