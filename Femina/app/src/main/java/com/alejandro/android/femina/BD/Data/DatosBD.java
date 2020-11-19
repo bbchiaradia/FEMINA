e7c12f36011cfe1e15d9fc63492ee4b0653c32d8
Alejandro081998 <ale.salgado08@hotmail.com>
2020-11-19 20:34:47 -0300

    Cambio base
    cambio base, porque la original cayó la página

--- Femina/app/src/main/java/com/alejandro/android/femina/BD/Data/DatosBD.java
+++ Femina/app/src/main/java/com/alejandro/android/femina/BD/Data/DatosBD.java
@@ -2,11 +2,17 @@ package com.alejandro.android.femina.BD.Data;
 
 public class DatosBD {
 
-    public static String host = "freedb.tech";
+    /*public static String host = "freedb.tech";
     public static String port = "3306";
     public static String nameBD = "freedbtech_femina_bd";
     public static String user = "freedbtech_femina2020";
-    public static String pass = "seminario2020";
+    public static String pass = "seminario2020";*/
+
+    public static String host = "remotemysql.com";
+    public static String port = "3306";
+    public static String nameBD = "evCEEWtuT5";
+    public static String user = "evCEEWtuT5";
+    public static String pass = "m0MAuseWxf";
 
     public static String urlMySQL = "jdbc:mysql://" + host + ":" + port + "/" + nameBD;
     public static String driver = "com.mysql.jdbc.Driver";
